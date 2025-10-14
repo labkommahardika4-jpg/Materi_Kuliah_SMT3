abf5bd751e43b65b87ea0daabb3e42b9b7a7c343
Mohamad Firdaus <mohamadfirdaus@uinssc.ac.id>
2025-10-14 14:17:04 +0700

    Pertemuan 6

---
+++ Pem_Web/Pertemuan-6/comparison.js
@@ -0,0 +1,13 @@
+// Comparison
+var a = 10;
+var b = '10';
+console.log(a == b);  // true, karena nilai sama
+console.log(a === b); // false, karena tipe berbeda
+
+console.log(a != b);  // false, karena nilai sama
+console.log(a !== b); // true, karena tipe berbeda
+
+console.log(1 > 5);   
+console.log(1 < 5);  
+console.log(5 >= 5);  
+console.log(5 <= 3);
